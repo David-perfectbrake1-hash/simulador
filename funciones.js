8710331ec753974184fe52b1a3eab8d30a5c858d
David R Git <perfectbrake1@gmail.com>
2026-04-16 19:49:10 -0500

    feat: add calcularCapacidadPago function

--- funciones.js
+++ funciones.js
@@ -1,8 +1,18 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+//funciones.js
+
 function calcularDisponible(ingresos, egresos) {
     let disponible = ingresos - egresos;
     if (disponible < 0) {
         return 0;
     }
     return disponible;
+}
+
+function calcularCapacidadPago(montoDisponible) {
+    // Multiplicamos el disponible por 0.50 (que es el 50%)
+    let capacidad = montoDisponible * 0.50;
+    
+    // Devolvemos el resultado
+    return capacidad;
 }
